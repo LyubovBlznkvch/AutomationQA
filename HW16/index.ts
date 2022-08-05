60f1e8f73c55b778261ee765fb7c94ed9af74ea2
Lyubov Bliznyukovich <lyubov.bliznyukovich@gmail.com>
2022-08-05 16:32:29 +0300

    HW16 is updated

--- HW16/index.ts
+++ HW16/index.ts
@@ -6,6 +6,8 @@ customChecker(10, 3, '-', 7);
 customChecker(3, 4, '*', 12);
 customChecker(12, 3, "/", 4);
 customChecker(3, 1, "**", 3);
+customChecker(45, 45, "/", 1);
+customChecker(3, 1, "+", 4);
 
 //negative tests
 customChecker(2, 2, '+', 5);
@@ -13,3 +15,5 @@ customChecker(10, 3, '-', 71);
 customChecker(3, 4, '*', 121);
 customChecker(12, 3, '/', 8);
 customChecker(3, 5, "**", 3);
+customChecker(12, 0, '/', 0);
+customChecker(3, 54, "*", 3);
