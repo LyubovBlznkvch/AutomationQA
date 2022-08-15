2048170646836b224e57e3b6bf516a04e303ecf2
Lyubov Bliznyukovich <lyubov.bliznyukovich@gmail.com>
2022-08-15 17:16:40 +0300

    HW17: jest-coverage is modified

--- HW17/mocha-test/calculator.test.ts
+++ HW17/mocha-test/calculator.test.ts
@@ -3,10 +3,10 @@ import { expect } from "chai";
 import { Calculator } from "../src/calculator";
 
 let calculator: any;
-const adderError: string = "Numbers are added incorrectly!";
-const subtractorError: string = "Numbers are subtracted incorrectly!";
-const multiplierError: string = "Numbers are multiplied incorrectly!";
-const deviderError: string = "Numbers are devided incorrectly!";
+const ADDER_ERROR: string = "Numbers are added incorrectly!";
+const SUBTRACTOR_ERROR: string = "Numbers are subtracted incorrectly!";
+const MULTIPLIER_ERROR: string = "Numbers are multiplied incorrectly!";
+const DEVIDER_ERROR: string = "Numbers are devided incorrectly!";
 
 
 describe("Tests for calculator", () => {
@@ -14,24 +14,24 @@ describe("Tests for calculator", () => {
         calculator = new Calculator;  
     });
     it("Should add numbers correctly", () => {
-        assert.equal(calculator.adder(3.3, 4), 7.3, adderError);
-        assert.equal(calculator.adder(3, -4), -1, adderError);
-        assert.equal(calculator.adder(0, -4), -4, adderError);
+        assert.equal(calculator.adder(3.3, 4), 7.3, ADDER_ERROR);
+        assert.equal(calculator.adder(3, -4), -1, ADDER_ERROR);
+        assert.equal(calculator.adder(0, -4), -4, ADDER_ERROR);
     });
     it("Should subtract numbers correctly", () => {
-        assert.equal(calculator.subtractioner(4.4, 4.4), 0, subtractorError);
-        assert.equal(calculator.subtractioner(-3, -4), 1, subtractorError);
-        assert.equal(calculator.subtractioner(3, 0), 3, subtractorError);
+        assert.equal(calculator.subtractioner(4.4, 4.4), 0, SUBTRACTOR_ERROR);
+        assert.equal(calculator.subtractioner(-3, -4), 1, SUBTRACTOR_ERROR);
+        assert.equal(calculator.subtractioner(3, 0), 3, SUBTRACTOR_ERROR);
     });
     it("Should multiply numbers correctly", () => {
-        assert.equal(calculator.multiplier(-3, -4), 12, multiplierError);
-        assert.equal(calculator.multiplier(3.5, 1), 3.5, multiplierError);
-        assert.equal(calculator.multiplier(0, 4), 0, multiplierError);
+        assert.equal(calculator.multiplier(-3, -4), 12, MULTIPLIER_ERROR);
+        assert.equal(calculator.multiplier(3.5, 1), 3.5, MULTIPLIER_ERROR);
+        assert.equal(calculator.multiplier(0, 4), 0, MULTIPLIER_ERROR);
     });
     it("Should divide numbers correctly", () => {
-        assert.equal(calculator.divider(12, 1.5), 8, deviderError);
-        assert.equal(calculator.divider(-12, 4), -3, deviderError);
-        assert.equal(calculator.divider(0, 4), 0, deviderError);
+        assert.equal(calculator.divider(12, 1.5), 8, DEVIDER_ERROR);
+        assert.equal(calculator.divider(-12, 4), -3, DEVIDER_ERROR);
+        assert.equal(calculator.divider(0, 4), 0, DEVIDER_ERROR);
     });
     after("Delete class instance", () => {
         calculator = null;
@@ -43,24 +43,24 @@ describe("Tests for calculator", () => {
         calculator = new Calculator;  ;  
     });
     it("Should add numbers correctly", () => {
-        expect(calculator.adder(3, -4)).to.be.deep.equal(-1, adderError);
-        expect(calculator.adder(3.3, 4)).to.be.deep.equal(7.3, adderError);
-        expect(calculator.adder(0, 4)).to.be.deep.equal(4, adderError);
+        expect(calculator.adder(3, -4)).to.be.deep.equal(-1, ADDER_ERROR);
+        expect(calculator.adder(3.3, 4)).to.be.deep.equal(7.3, ADDER_ERROR);
+        expect(calculator.adder(0, 4)).to.be.deep.equal(4, ADDER_ERROR);
     });
     it("Should subtract numbers correctly", () => {
-        expect(calculator.subtractioner(3, -4)).to.be.deep.equal(7, subtractorError);
-        expect(calculator.subtractioner(5.4, 4.4)).to.be.deep.equal(1, subtractorError);
-        expect(calculator.subtractioner(0, 4)).to.be.deep.equal(-4, subtractorError);
+        expect(calculator.subtractioner(3, -4)).to.be.deep.equal(7, SUBTRACTOR_ERROR);
+        expect(calculator.subtractioner(5.4, 4.4)).to.be.deep.equal(1, SUBTRACTOR_ERROR);
+        expect(calculator.subtractioner(0, 4)).to.be.deep.equal(-4, SUBTRACTOR_ERROR);
     });
     it("Should multiply numbers correctly", () => {
-        expect(calculator.multiplier(3, -4)).to.be.deep.equal(-12, multiplierError);
-        expect(calculator.multiplier(3, 1.5)).to.be.deep.equal(4.5, multiplierError);
-        expect(calculator.multiplier(0, -4)).to.be.deep.equal(0, multiplierError);
+        expect(calculator.multiplier(3, -4)).to.be.deep.equal(-12, MULTIPLIER_ERROR);
+        expect(calculator.multiplier(3, 1.5)).to.be.deep.equal(4.5, MULTIPLIER_ERROR);
+        expect(calculator.multiplier(0, -4)).to.be.deep.equal(0, MULTIPLIER_ERROR);
     });
     it("Should divide numbers correctly", () => {
-        expect(calculator.divider(12, -4)).to.be.deep.equal(-3, deviderError);
-        expect(calculator.divider(12, 8)).to.be.deep.equal(1.5, deviderError);
-        expect(calculator.divider(0, 4)).to.be.deep.equal(0, deviderError);
+        expect(calculator.divider(12, -4)).to.be.deep.equal(-3, DEVIDER_ERROR);
+        expect(calculator.divider(12, 8)).to.be.deep.equal(1.5, DEVIDER_ERROR);
+        expect(calculator.divider(0, 4)).to.be.deep.equal(0, DEVIDER_ERROR);
     });
     after("Delete class instance", () => {
         calculator = null;
